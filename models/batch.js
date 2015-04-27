f6d9ebf8bcfb5a6365fde15a37c1cc6a8329b4d8
Brad Dawson <wbd143@gmail.com>
2015-04-27 18:41:31 -0500

    added batches.js

---
+++ models/batch.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+
+var batchSchema = mongoose.Schema({
+    cook_date: {type: Date, required: true, default: Date.now },
+    exp_date: {type: Date, required: true, default: '' },
+    notes: {type: String, required: true, default: ''},
+    candyType: {type: String, required: true, default: ''},
+    sugarId: {type: String, required: true, default: ''},
+    butterId: {type: String, required: true, default: ''},
+    almondId: {type: String, required: true, default: ''},
+    chocolateId: {type: String, required: true, default: ''},
+    dustId: {type: String, required: true, default: ''},
+    cook_order: {type: Number, required: true, default: '' },
+    user: {type: String, required: true}
+});
+
+var Batch = mongoose.model('Batch', batchSchema);
+
+module.exports = Batch;
