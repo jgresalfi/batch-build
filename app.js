991a964db2007c02e5e6502b41f72f51736b9118
Jason Gresalfi <jason.gresalfi@gmail.com>
2015-04-27 14:08:10 -0500

    Fixed broken template variable and assigned global title var in app.js

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ var app = express();
 
 var db = require('./db_connect');
 
+//global template variables
+app.locals.title = "Batch Builder";
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
