a6ecf9a33e0efd903924c2ed8746fef6319fa361
Jason Gresalfi <jason.gresalfi@gmail.com>
2015-04-27 14:36:26 -0500

    Modified database path in db_connect.js

--- db_connect.js
+++ db_connect.js
@@ -1,7 +1,7 @@
 // Mongoose connection to MongoDB
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
+mongoose.connect('mongodb://localhost/batch');
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
