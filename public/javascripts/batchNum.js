7860386a36738e4fa03c8cb45298dc822609ebc6
Brad Dawson <wbd143@gmail.com>
2015-05-05 12:37:32 -0500

    added type of candy to batchNum.js

--- public/javascripts/batchNum.js
+++ public/javascripts/batchNum.js
@@ -1,8 +1,16 @@
-var elem = document.getElementById("batchNum");
-Date.prototype.getJulian = function() {
-    return Math.floor((this / 86400000) - (this.getTimezoneOffset()/1440) + 2440587.5);
-}
 
-var today = new Date();
-var julian = today.getJulian();
-elem.value = julian;
+$( document ).ready(function() {
+  $( "select" ).change(function () {
+    var str = "";
+    Date.prototype.getJulian = function() {
+        return Math.floor((this / 86400000) - (this.getTimezoneOffset()/1440) + 2440587.5);
+    }
+    var today = new Date();
+    var julian = today.getJulian();
+    $( "select option:selected" ).each(function() {
+      str += $( this ).text() + " ";
+    });
+    $( "#batchNum" ).val( julian + str );
+  })
+  .change();
+});
